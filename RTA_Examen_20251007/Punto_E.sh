b9e8f9ca16aa9e3444d282898d394005656083e9
Santiago Iturrarte <Santiagojoeliturrarte@gmail.com>
2025-10-08 23:03:58 +0000

    feat: punto E y su script

--- RTA_Examen_20251007/Punto_E.sh
+++ RTA_Examen_20251007/Punto_E.sh
@@ -12,8 +12,8 @@ sudo chown p1c1_2025_u3:p1c1_2025_gTodos /Punto_D/Videos
 echo "ajustar permisos de las carpetas"
 
 sudo chmod 710 /Punto_D/Libros
-sudo chmod 704 /Punto_D/Musica
-sudo chmod 755 /Punto_D/Videos
+sudo chmod 745 /Punto_D/Musica
+sudo chmod 775 /Punto_D/Videos
 
 echo "verificar"
 
